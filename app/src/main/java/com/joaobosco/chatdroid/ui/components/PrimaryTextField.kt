6827a2cf772b5ad35bd5d3ed646dcbc919a7a3d5
Junior <ti.junior@gmail.com>
2024-09-09 20:18:13 -0300

    Atualizando o text field

--- app/src/main/java/com/joaobosco/chatdroid/ui/components/PrimaryTextField.kt
+++ app/src/main/java/com/joaobosco/chatdroid/ui/components/PrimaryTextField.kt
@@ -1,18 +1,26 @@
 package com.joaobosco.chatdroid.ui.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -34,8 +42,14 @@ fun PrimaryTextField(
     @DrawableRes
     leadingIcon: Int? = null,
     keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
     errorMessage: String? = null
 ) {
+
+    var passwordVisible by remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = modifier
     ) {
@@ -57,18 +71,35 @@ fun PrimaryTextField(
             },
             trailingIcon = {
                 if (keyboardType == KeyboardType.Password && value.isNotEmpty()) {
+                    val visibilityIcon = if (passwordVisible) {
+                        R.drawable.ic_visibility
+                    } else {
+                        R.drawable.ic_visibility_off
+                    }
                     Icon(
-                        painter = painterResource(id = R.drawable.ic_visibility_off),
+                        painter = painterResource(id = visibilityIcon),
                         contentDescription = null,
+                        modifier = Modifier
+                            .clickable {
+                                passwordVisible = !passwordVisible
+                            },
                         tint = MaterialTheme.colorScheme.primary
                     )
                 }
             },
             visualTransformation = if (keyboardType == KeyboardType.Password) {
-                PasswordVisualTransformation()
+                if (passwordVisible) {
+                    VisualTransformation.None
+                } else {
+                    PasswordVisualTransformation()
+                }
             } else {
                 VisualTransformation.None
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
             singleLine = true,
             shape = CircleShape,
             colors = OutlinedTextFieldDefaults.colors(
--- app/src/main/java/com/joaobosco/chatdroid/ui/feature/signin/SignInScreen.kt
+++ app/src/main/java/com/joaobosco/chatdroid/ui/feature/signin/SignInScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,7 +82,8 @@ fun SignInScreen() {
                 .padding(horizontal = 16.dp),
             placeholder = stringResource(id = R.string.feature_login_password),
             leadingIcon = R.drawable.ic_lock,
-            keyboardType = KeyboardType.Password
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
         )
     }
 }
