95c1edab47f003423cc27d385ce9a2e433faf265
Junior <ti.junior@gmail.com>
2024-12-09 21:56:08 -0300

    Configurando o Dagger Hilt no projeto

---
+++ app/src/main/java/com/joaobosco/chatdroid/ChatDroidApp.kt
@@ -0,0 +1,11 @@
+package com.joaobosco.chatdroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by "João Bosco" on 09/12/2024.
+ */
+
+@HiltAndroidApp
+class ChatDroidApp : Application()
--- app/src/main/java/com/joaobosco/chatdroid/MainActivity.kt
+++ app/src/main/java/com/joaobosco/chatdroid/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.joaobosco.chatdroid.ui.ChatApp
 import com.joaobosco.chatdroid.ui.theme.ChatDroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
